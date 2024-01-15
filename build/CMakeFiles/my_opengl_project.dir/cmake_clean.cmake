40c5fbd9184bf3bcfbd3b6b3e6de3a0009c57416
AlternateFire <dsyang04@gmail.com>
2024-01-15 18:40:54 -0500

    shit

---
+++ build/CMakeFiles/my_opengl_project.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_opengl_project.dir/src/main.cpp.o"
+  "CMakeFiles/my_opengl_project.dir/src/main.cpp.o.d"
+  "my_opengl_project"
+  "my_opengl_project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/my_opengl_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
